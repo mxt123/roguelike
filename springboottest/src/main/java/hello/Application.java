c7f86f8cf869962ded897ab12efffa2a5f140235
Matt Toseland <m.toseland@validus-ivc.co.uk>
2017-06-14 15:24:30 +0100

    springboottest

---
+++ springboottest/src/main/java/hello/Application.java
@@ -0,0 +1,80 @@
+package hello;
+
+import org.h2.util.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			//read json file data to String
+			Path path = Paths.get("resources/customer.json");
+			byte[] jsonData = Files.readAllBytes(path);
+			
+			//create ObjectMapper instance
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			//convert json string to object
+			Customer cust1 = objectMapper.readValue(jsonData, Customer.class);
+						
+			repository.save(cust1);
+			
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Bauer")) {
+				log.info(bauer.toString());
+			}
+			log.info("");
+			
+			// fetch customers by last name
+			log.info("--------------------------------------------");
+			for (Customer hat : repository.findByLastName("hat")) {
+				log.info(hat.toString());
+			}
+			log.info("");
+		};
+	}
+
+}
